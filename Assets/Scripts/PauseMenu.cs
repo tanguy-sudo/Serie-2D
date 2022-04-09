bb6a35c1948280019f68b0e908d648b59302468b
tanguy <tanguy.jouvin@hotmail.com>
2022-04-09 19:18:56 +0200

    ajout menu pause

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+
+        }
+    }
+
+    public void Resume()
+    {
+        PlayerMovement.instance.enabled = true;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        gameIsPaused = false;
+    }
+ 
+    private void Paused()
+    {
+        // Empeche les mouvements du joueur
+        PlayerMovement.instance.enabled = false;
+        pauseMenuUI.SetActive(true);
+        // stoppe le temps
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+    public void loadMainMenu()
+    {
+        DontDestroyOnLoadScene.instance.RemoveFromDontDestroyOnLoad();
+        Resume();
+        SceneManager.LoadScene("MainMenu");
+    }
+}
